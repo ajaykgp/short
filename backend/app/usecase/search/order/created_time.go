f0324ddec182ff485d25b52d8772cb36686ea929
Arber Avdullahu <arber_919@live.com>
2020-06-03 20:36:00 +0200

    Update created_time.go

--- backend/app/usecase/search/order/created_time.go
+++ backend/app/usecase/search/order/created_time.go
@@ -8,26 +8,30 @@ import (
 
 var _ Order = (*CreatedTime)(nil)
 
-// CreatedTime represents the order of resources based on created time
+// CreatedTime arranges searchable resources based on their created time.
 type CreatedTime struct {
 }
 
-// ArrangeShortLinks sorts users based on time of creation
+// ArrangeShortLinks arranges shortLinks based on CreatedAt.
 func (c CreatedTime) ArrangeShortLinks(shortLinks []entity.ShortLink) []entity.ShortLink {
 	sort.SliceStable(shortLinks, func(i, j int) bool {
 		if shortLinks[i].CreatedAt == nil {
 			return true
+		} else if shortLinks[j].CreatedAt == nil {
+			return false
 		}
 		return shortLinks[i].CreatedAt.Before(*shortLinks[j].CreatedAt)
 	})
 	return shortLinks
 }
 
-// ArrangeUsers sorts users based on time of creation
+// ArrangeUsers arranges users based on CreatedAt.
 func (c CreatedTime) ArrangeUsers(users []entity.User) []entity.User {
 	sort.SliceStable(users, func(i, j int) bool {
 		if users[i].CreatedAt == nil {
 			return true
+		} else if users[j].CreatedAt == nil {
+			return false
 		}
 		return users[i].CreatedAt.Before(*users[j].CreatedAt)
 	})
